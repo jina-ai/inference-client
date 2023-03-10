7a2e5211a7552f07ddf80140292358826199882b
ZiniuYu <ziniuyu@gmail.com>
2023-03-10 16:48:38 +0800

    feat: inference client login

---
+++ inference_client/__init__.py
@@ -0,0 +1,1 @@
+from .inference_client import InferenceClient
--- inference_client/inference_client.py
+++ inference_client/inference_client.py
@@ -1,1 +1,22 @@
-from jina import Client
+import os
+
+import hubble
+from hubble.utils.auth import Auth
+
+
+class InferenceClient:
+    def __init__(self, token: str = None):
+        print('InferenceClient.__init__()')
+        if token:
+            os.environ['JINA_AUTH_TOKEN'] = token
+            Auth.validate_token(token)
+        else:
+            print('InferenceClient.__init__() - calling hubble.login()')
+            hubble.login()
+            print('InferenceClient.__init__() - calling hubble.get_token()')
+            token = hubble.get_token()
+            print('InferenceClient.__init__() - token:', token)
+
+
+if __name__ == '__main__':
+    ic = InferenceClient(token='asdfasdf')
