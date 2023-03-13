ab240ef0b6ca99917d03093e24aff6265e29e6b3
ZiniuYu <ziniuyu@gmail.com>
2023-03-13 16:21:40 +0800

    fix: address comments

--- client/client.py
+++ client/client.py
@@ -1,8 +1,6 @@
-import os
 from typing import Optional
 
-import hubble
-from hubble.utils.auth import Auth
+from .helper import get_model, login
 
 
 class Client:
@@ -20,44 +18,5 @@ class Client:
         :return: None
         """
         self.model_name = model_name
-        self.token = self._login(token=token)
-        self._connect(self.model_name)
-
-    def _login(self, token: Optional[str] = None):
-        """
-
-        :param token: A optional token to use for authentication. If not set, it will try to login using the auth token
-        in the cache or env, or guide the user to login from a pop-out window
-        :return: The token used for authentication.
-        """
-        if token:
-            os.environ['JINA_AUTH_TOKEN'] = token
-            Auth.validate_token(token)
-            return token
-        else:
-            hubble.login()
-            return hubble.get_token()
-
-    def _connect(self, model_name):
-        """
-        Validate whether the user has access to the specified model. Retrieves metadata for the specified model.
-
-        :param model: The name of the model to connect to.
-        :return: None.
-        """
-        cfg = self._fetch_metadata(model_name)
-        print(cfg)
-
-    def _fetch_metadata(self, model_name):
-        """
-        Retrieves metadata for the specified model.
-
-        :param model: The name of the model to retrieve metadata for.
-        :return: A dictionary containing metadata for the model.
-        """
-        print(f'fetching metadata for {model_name}')
-        return {
-            'grpc': 'grpcs://api.clip.jina.ai:2096',
-            'http': 'https://api.clip.jina.ai:8443',
-            'image_size': 224,
-        }
+        self.token = login(token)
+        self.cfg = get_model(self.token, self.model_name)
--- tests/test_login.py
+++ tests/test_login.py
@@ -6,29 +6,29 @@ import pytest
 from client import Client
 
 
-@patch('client.Client._login', Mock(return_value='valid token'))
-@patch('client.Client._fetch_metadata', Mock(return_value={'model': 'model name'}))
+@patch('client.client.login', Mock(return_value='valid token'))
+@patch('client.helper.fetch_metadata', Mock(return_value={'model': 'model name'}))
 def test_valid_token():
-    client = Client(model='model name', token='valid token', force=True)
+    client = Client(model_name='model name', token='valid token')
     assert client.token == 'valid token'
 
 
 def test_invalid_token():
     with pytest.raises(Exception):
-        Client(model='model name', token='invalid token', force=True)
+        Client(model_name='model name', token='invalid token')
 
 
 @patch.dict(os.environ, {'JINA_AUTH_TOKEN': 'valid session'})
-@patch('client.Client._login', Mock(return_value='valid session'))
-@patch('client.Client._fetch_metadata', Mock(return_value={'model': 'model name'}))
+@patch('client.client.login', Mock(return_value='valid session'))
+@patch('client.helper.fetch_metadata', Mock(return_value={'model': 'model name'}))
 def test_no_token_valid_session():
-    client = Client(model='model name')
+    client = Client(model_name='model name')
     assert client.token == 'valid session'
 
 
 @patch.dict(os.environ, {'JINA_AUTH_TOKEN': 'invalid session'})
-@patch('client.Client._login', Mock(return_value='valid session'))
-@patch('client.Client._fetch_metadata', Mock(return_value={'model': 'model name'}))
+@patch('client.client.login', Mock(return_value='valid session'))
+@patch('client.helper.fetch_metadata', Mock(return_value={'model': 'model name'}))
 def test_no_token_invalid_session():
-    client = Client(model='model name')
+    client = Client(model_name='model name')
     assert client.token == 'valid session'
