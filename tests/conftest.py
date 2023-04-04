56f3d889c20864c9cd53fde66a972fe1cb3968f3
ZiniuYu <ziniuyu@gmail.com>
2023-04-03 21:50:54 -0400

    chore: housekeeping

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,1 @@
 import pytest
-
-
-@pytest.fixture(scope='session', params=['clip', 'blip2'])
-def make_model_name_and_host(request):
-    if request.param == 'blip2':
-        return (
-            'Salesforce/blip2-opt-2.7b',
-            'grpcs://crucial-gazelle-779d1c8739-grpc.wolf.jina.ai',
-        )
-    else:
-        return 'ViT-B-32::openai', 'grpcs://api.clip.jina.ai:2096'
-
-
-@pytest.fixture(scope='session')
-def make_inference():
-    return 'inference-model-name', 'grpcs://model-name.inference.jina.ai'
