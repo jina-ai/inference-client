ea016ac86812f33fa8a9cf6de5dbd511e63a0f71
ZiniuYu <ziniuyu@gmail.com>
2023-04-24 17:19:08 +0800

    docs: fix logos

--- docs/conf.py
+++ docs/conf.py
@@ -38,8 +38,8 @@ sitemap_locales = [None]
 sitemap_filename = "sitemap.xml"
 
 html_theme_options = {
-    'light_logo': 'logo-light.svg',
-    'dark_logo': 'logo-dark.svg',
+    'light_logo': 'inference_client.svg',
+    'dark_logo': 'inference_client.svg',
     "sidebar_hide_name": True,
     "light_css_variables": {
         "color-brand-primary": "#009191",
