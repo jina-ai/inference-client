0609fa5ca4e3931f98d29854d63bef54fc57ddd1
numb3r3 <wangfelix87@gmail.com>
2023-05-19 14:03:16 +0800

    feat: add generation mixin

--- inference_client/client.py
+++ inference_client/client.py
@@ -29,26 +29,23 @@ class Client:
             ) from None
 
     @lru_cache(maxsize=10)
-    def get_model(self, model_name_or_endpoint: str):
+    def get_model(
+        self, model_name: Optional[str] = None, endpoint: Optional[str] = None
+    ):
         """
         Get a model by name or endpoint. Returns a cached model if it exists.
 
-        :param model_name_or_endpoint: The name of the model or the endpoint.
+        :param model_name: The name of the model.
+        :param endpoint: The endpoint of the model.
         :return: The model.
         """
 
-        from urllib.parse import urlparse
-
-        scheme = urlparse(model_name_or_endpoint).scheme
-
-        if not scheme:
-            spec = get_model_spec(model_name_or_endpoint, self._auth_token)
+        if model_name:
+            spec = get_model_spec(model_name, self._auth_token)
             endpoint = spec['endpoints']['grpc']
-        else:
-            endpoint = model_name_or_endpoint
 
         return BaseClient(
-            model_name=model_name_or_endpoint,
+            model_name=model_name,
             token=self._auth_token,
             host=endpoint,
         )
