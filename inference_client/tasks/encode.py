2f735d4f26bb9a2bb0f679f079ffcdf901bff92e
ZiniuYu <ziniuyu@gmail.com>
2023-07-04 11:34:39 +0800

    fix: gh action

--- inference_client/tasks/encode.py
+++ inference_client/tasks/encode.py
@@ -236,9 +236,9 @@ class EncodeMixin:
                 'Please provide either text, image or docs input to encode.'
             )
 
-        # payload.update(on_done=kwargs.pop('on_done', None))
-        # payload.update(on_error=kwargs.pop('on_error', None))
-        # payload.update(on_always=kwargs.pop('on_always', None))
+        payload.update(on_done=kwargs.pop('on_done', None))
+        payload.update(on_error=kwargs.pop('on_error', None))
+        payload.update(on_always=kwargs.pop('on_always', None))
         # payload.update(prefetch=kwargs.pop('prefetch', 100))
 
         return payload, content_type, is_list
